d12f5770b5f408b7d72327fd4844ff97a34abedf
1298743328szyasy <1298743328@qq.com>
2019-09-09 21:05:20 +0800

    随机化快速排序无法尽快排列大量重复的数据，双路快排避免了选定随机数与排列数相同时，两端的数不平衡的问题 继续加油：）

--- main/main.cpp
+++ main/main.cpp
@@ -6,15 +6,20 @@
 #include "../header/SortTestHelper.h"
 #include "../header/insertionSort.h"
 #include "../header/mergeSort.h"
+#include <ctime>
 using namespace std;
 template<typename T>
 int __partion(T arr[],int l,int r){
+    swap(arr[l],arr[rand()%(r-l+1)+l]);
     T v = arr[l];
-    //[l+1...j]  [j+1...i]
-    int j = l;
-    for(int i=l+1;i<=r;i++){
-        if(arr[i]<v)
-            swap(arr[i],arr[++j]);
+    int i=l+1,j=r;
+    while(true){
+        while(i<=r&&arr[i]<v)i++;
+        while(j>=l+1&&arr[j]>v)j--;
+        if(i>j)break;
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
     }
     swap(arr[l],arr[j]);
     return j;
@@ -29,14 +34,22 @@ void __quickSort(T arr[],int l,int r){
 }
 template<typename T>
 void quickSort(T arr[],int n){
+    srand(time(NULL));
     __quickSort(arr,0,n-1);
 }
 int main(){
-    int n=50000;
-    int *arr1=SortTestHelper::generateRandomArray(n,0,n);
-    int *arr2=SortTestHelper::copyIntArray(arr1,n);
-    SortTestHelper::testSort("InsertionSort",insertionSort,arr1,n);
-    SortTestHelper::testSort("merge Sort",mergeSort,arr2,n);
+    int n=1000000;
+    static int *arr1=SortTestHelper::generateRandomArray(n,0,10);
+//    int *arr2=SortTestHelper::copyIntArray(arr1,n);
+//    SortTestHelper::testSort("merge Sort",mergeSort,arr1,n);
+    SortTestHelper::testSort("quick Sort",quickSort,arr1,n);
     delete[] arr1;
-    delete[] arr2;
+//    delete[] arr2;
+//    int swapTimes=100;
+//    arr1=SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
+//    arr2=SortTestHelper::copyIntArray(arr1,n);
+//    SortTestHelper::testSort("merge Sort",mergeSort,arr1,n);
+//    SortTestHelper::testSort("quick Sort",quickSort,arr2,n);
+//    delete[] arr1;
+//    delete[] arr2;
 }
